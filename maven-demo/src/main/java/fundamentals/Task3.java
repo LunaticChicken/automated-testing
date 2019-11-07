8337588e8348b71d7471e95f4161a3e503b11220
Vitali Snitko <vitalisnitko228@gmail.com>
2019-11-07 19:08:47 +0300

    Update Task3

--- maven-demo/src/main/java/fundamentals/Task3.java
+++ maven-demo/src/main/java/fundamentals/Task3.java
@@ -2,9 +2,9 @@ package fundamentals;
 
 public class Task3 {
     public static void main(String[] args) {
-        int amountOfNumbers = Integer.parseInt(args[0]);
+        int amountOfRows = Integer.parseInt(args[0]);
         System.out.println("Random numbers from 0 to 9:");
-        for (int i = 0; i < amountOfNumbers; i++) {
+        for (int i = 0; i < amountOfRows; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print((int)(Math.random()*10) + " ");
             }
